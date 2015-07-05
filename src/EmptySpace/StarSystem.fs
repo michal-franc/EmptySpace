4663f4ab9ac40ce8d6844c201000f0534a737473
Michal Franc <lam.michal.franc@gmail.com>
2015-07-06 00:37:06 +0100

    Moving out SpriteWithHint to separate file

--- src/EmptySpace/StarSystem.fs
+++ src/EmptySpace/StarSystem.fs
@@ -3,6 +3,7 @@ open SFML.Graphics
 open SFML.Window
 open Helper
 open TopBar
+open SpriteWithHint
 
 type ObjectType = Sun | Planet | Moon | Asteroid
 type Size = Huge | Large | Medium | Small | Tiny
@@ -66,38 +67,7 @@ type StarSystem = {
         let x, y = planetSpriteStart planetType
         new IntRect(x + xForNthPlanet, y, 30, 30)
 
- type SpriteWithHint(hint, texture, intRect) =
-    inherit Sprite(texture, intRect) with
-    let hint = hint
 
-    member this.DrawHint(window:RenderWindow) =
-        let mPos = Mouse.GetPosition(window)
-        if this.MouseInSprite(mPos) then
-            let t, b = this.HintTextRect()
-            window.Draw(b)
-            window.Draw(t)
-        
-
-    member this.MouseInSprite(vector:Vector2i) =
-        // cant belevie that it works from the start without any bugs :D
-        // this is a little (hack) but it kinda works
-        // create a little rect based on mouse pos and then just do a intersect
-        // BUG: zoom out ( change in view ) causes this to fail ;)
-        let test = new FloatRect(float32(vector.X), float32(vector.Y), 1.0f, 1.0f)
-        this.GetGlobalBounds().Intersects(test)
-
-    member this.HintTextRect() =
-        let mutable back = new RectangleShape(vector (120.0f, 20.0f))
-        back.Position <-  addPos (20.0f, 0.0f) (this.Position.X, this.Position.Y) |> vector
-        back.FillColor <- Color.Yellow
-
-        let h = new Text(hint, Font)
-        h.Scale <- vector1 (0.4f)
-        let pos = centerPos (back.GetGlobalBounds())
-        h.Position <- adjustPos pos (h.GetGlobalBounds()) |> vector
-        h.Color <- Color.Black
-
-        (h, back)
 
 //TODO: sprite number based on some RNG
 type SystemDrawRenderer(filename:string) =
@@ -107,7 +77,7 @@ type SystemDrawRenderer(filename:string) =
     member this.Sprites with get() = sprites
 
     member this.sprite pos size t name =
-        let s = new SpriteWithHint(name, texture, rectForType 1 t)
+        let s = new SpriteWithHint(name, texture, rectForType 1 t, Font)
         s.Scale <- vector1 size 
         s.Position <- adjustPos pos (s.GetGlobalBounds()) |> vector 
         s
---
+++ src/EmptySpace/Views/Elements/SpriteWithHint.fs
@@ -0,0 +1,39 @@
+﻿module SpriteWithHint
+open SFML.Graphics
+open SFML.Window
+open Helper
+
+ type SpriteWithHint(hint, texture, intRect, font) =
+    inherit Sprite(texture, intRect) with
+    let hint = hint
+    let font = font
+
+    member this.DrawHint(window:RenderWindow) =
+        let mPos = Mouse.GetPosition(window)
+        if this.MouseInSprite(mPos) then
+            let t, b = this.HintTextRect()
+            window.Draw(b)
+            window.Draw(t)
+        
+
+    member this.MouseInSprite(vector:Vector2i) =
+        // cant belevie that it works from the start without any bugs :D
+        // this is a little (hack) but it kinda works
+        // create a little rect based on mouse pos and then just do a intersect
+        // BUG: zoom out ( change in view ) causes this to fail ;)
+        let test = new FloatRect(float32(vector.X), float32(vector.Y), 1.0f, 1.0f)
+        this.GetGlobalBounds().Intersects(test)
+
+    member this.HintTextRect() =
+        let mutable back = new RectangleShape(vector (120.0f, 20.0f))
+        back.Position <-  addPos (20.0f, 0.0f) (this.Position.X, this.Position.Y) |> vector
+        back.FillColor <- Color.Yellow
+
+        let h = new Text(hint, font)
+        h.Scale <- vector1 (0.4f)
+        let pos = centerPos (back.GetGlobalBounds())
+        h.Position <- adjustPos pos (h.GetGlobalBounds()) |> vector
+        h.Color <- Color.Black
+
+        (h, back)
+
