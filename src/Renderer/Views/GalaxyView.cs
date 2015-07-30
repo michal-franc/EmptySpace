d3148abc221b4df9cb43a68aa145ee7c87e7706e
Michal Franc <lam.michal.franc@gmail.com>
2015-07-30 22:08:00 +0100

    Added basic galaxy map scrolling

--- src/Renderer/Views/GalaxyView.cs
+++ src/Renderer/Views/GalaxyView.cs
@@ -1,12 +1,41 @@
-﻿using Renderer.Controls.Buttons;
+﻿using System;
+using Renderer.Controls.Buttons;
 using SFML.Graphics;
 using SFML.Window;
 
 namespace Renderer.Views
 {
+    public static class Vector2fExtensions
+    {
+        public static bool IsZero(this Vector2f vect)
+        {
+            return vect.X == 0.0f && vect.Y == 0.0f;
+        }
+
+        public static Vector2f Apply(this Vector2f vect, Func<float, float> apply)
+        {
+            vect.X = apply(vect.X);
+            vect.Y = apply(vect.Y);
+            return vect;
+        }
+
+        public static Vector2f Normalize(this Vector2f vect)
+        {
+            Func<float, float> abs = (val) =>
+            {
+                var wasNegative = val > 0.0f;
+                val = (float) Math.Sqrt(Math.Abs(val));
+                return wasNegative ? val : -val;
+            };
+
+            return vect.Apply(abs);
+        }
+    }
+
     public class GalaxyView: GameView
     {
         public override string Name => "Galaxy";
+        private Vector2f _mouseDragStartPosition;
 
         private View mainView;
 
@@ -28,14 +57,50 @@ namespace Renderer.Views
             target.Draw(GlobalAssets.SpaceBackground);
 
             var window = ((RenderWindow)target);
+
+            // Those are hacks for testing :)
             window.MouseWheelMoved -= MoseWheelMoved;
             window.MouseWheelMoved += MoseWheelMoved;
 
             window.KeyPressed -= WindowOnKeyPressed;
             window.KeyPressed += WindowOnKeyPressed;
 
+            window.MouseButtonPressed -= OnWindowOnMouseButtonPressed;
+            window.MouseButtonPressed += OnWindowOnMouseButtonPressed;
+
+            window.MouseMoved -= OnWindowOnMouseMoved;
+            window.MouseMoved += OnWindowOnMouseMoved;
+
             target.SetView(mainView);
             base.Draw(target, states);
+        }
+
+        private void OnWindowOnMouseMoved(object sender, MouseMoveEventArgs args)
+        {
+            if (Mouse.IsButtonPressed(Mouse.Button.Right))
+            {
+                var curPos = new Vector2f(args.X, args.Y);;
+
+                var diff = _mouseDragStartPosition - curPos;
+                if (!diff.IsZero())
+                {
+                    var multiplier = 2.0f + (this.mainView.Size.X / 1000.0f) ;
+                    diff = diff.Normalize();
+                    diff *= multiplier;
+
+                    this.mainView.Move(diff);
+                }
+
+                _mouseDragStartPosition = curPos;
+            }
+        }
+
+        private void OnWindowOnMouseButtonPressed(object sender, MouseButtonEventArgs args)
+        {
+            if (args.Button == Mouse.Button.Right)
+            {
+                _mouseDragStartPosition = new Vector2f(args.X, args.Y);
+            }
         }
 
         private void MoseWheelMoved(object sender, MouseWheelEventArgs args)
