9c6e0152f73bbe0e4c56b66a29e41720f8dfcf45
Michal Franc <lam.michal.franc@gmail.com>
2015-08-09 20:34:30 +0100

    Crappy implementation of fuel consumption when 'flying'

--- src/Engine/GameEngine.fs
+++ src/Engine/GameEngine.fs
@@ -28,19 +28,28 @@ let create = fun () ->
 let tick state =
     let (crewlist, storage) = state.CrewList.tick(state.Storage)
 
+    //TODO: Move this whole logic to SHIP tick
+    //TODO: BUG - Slight mismatch beetwen dest and position ( it wont reach 0.0f ) -> introduce either error rate or fix the positioning
     let speed = 0.5f;
 
-    //TODO: Move this whole logic to SHIP tick
-    let whereToGo = state.PlayerPosition - state.Destination;
+    let mutable whereToGo = state.PlayerPosition - state.Destination;
+    //DOH: this will be prone to bugs hahahah
+    if abs whereToGo.X < 0.3f && whereToGo.X <> 0.0f then
+        whereToGo <- vector (0.0f, whereToGo.Y)
+
+    if abs whereToGo.Y < 0.3f && whereToGo.Y <> 0.0f then
+        whereToGo <- vector (whereToGo.X, 0.0f)
+
     let moveVector = (normalize whereToGo) * speed;
-    if moveVector.X > 0.0f || moveVector.Y > 0.0f then
-        let (newStorage, x) = Storage.take "Fuel" 1 storage
-        if x > 0 then
+
+    if moveVector.X <> 0.0f || moveVector.Y <> 0.0f then
+        let (newStorage, x) = Storage.take "Fuel" 0.01f storage
+        if x > 0.0f then
             { state with Storage = newStorage; CrewList = crewlist; PlayerPosition = state.PlayerPosition - moveVector }
         else
             { state with Storage = newStorage; CrewList = crewlist; PlayerPosition = state.PlayerPosition }
     else
-        { state with Storage = storage; CrewList = crewlist; PlayerPosition = state.PlayerPosition - moveVector }
+        { state with Storage = storage; CrewList = crewlist; PlayerPosition = state.PlayerPosition }
 
 
 let startTravel state pos = 
