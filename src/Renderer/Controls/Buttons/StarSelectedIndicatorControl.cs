5820c2681fa8aff18b5133e520dee764d4b0edbd
Michal Franc <lam.michal.franc@gmail.com>
2016-08-19 00:23:15 +0100

    fixed positioning of star selection indicator

--- src/Renderer/Controls/Buttons/StarSelectedIndicatorControl.cs
+++ src/Renderer/Controls/Buttons/StarSelectedIndicatorControl.cs
@@ -4,17 +4,17 @@ using SFML.Window;
 
 namespace Renderer.Controls.Buttons
 {
-    //TODO: Positioning of this selection is wrong
     //TODO: Some nice sprite for this
+    //TODO: position of this seleciton is dependand on star size
     public class StarSelectedIndicatorControl : IBaseControl
     {
         private Shape _shape;
         public FloatRect GlobalBounds => _shape.GetGlobalBounds();
 
-        public StarSelectedIndicatorControl(Vector2f playerPosition)
+        public StarSelectedIndicatorControl(Vector2f starPosition)
         {
             var r = 20.0f;
-            var middle = playerPosition - new Vector2f(r, r);
+            var middle = starPosition - new Vector2f((r / 2) + 2, (r / 2) + 2);
             _shape = new CircleShape(r);
             _shape.Position = middle;
             _shape.OutlineColor = new Color(255, 0, 0, 150);
